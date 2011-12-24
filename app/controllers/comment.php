117f7fba9a66f07a0895b1b99f482e8c07ee0e73
Chris Dukakis <chrisdukakis@gmail.com>
2011-12-24 20:28:31 +0200

    Update app/controllers/comment.php

--- app/controllers/comment.php
+++ app/controllers/comment.php
@@ -2,7 +2,10 @@
 class CommentController extends comment { 
 	
 	public function post() {
-		if ( isset( $_SESSION['user'] ) && !empty( $this->text ) || ( !isset( $_SESSION['user'] ) && !empty( $this->full_name ) ) ) {
+		if ( isset( $_SESSION['user'] ) && 
+		     !empty( $this->text ) || 
+		     ( !isset( $_SESSION['user'] ) 
+		       && !empty( $this->full_name ) ) ) {
 			if ( isset( $_SESSION['user'] ) ) {
 				$this->username = $_SESSION['user']; 
 				$this->assoc_id = $_GET['id']; 
