c949aebe21b2a1f7584b47d5ce591a9c147ba303
Chris Dukakis <chrisdukakis@gmail.com>
2012-02-08 20:12:19 +0200

    Update app/settings.php

--- app/settings.php
+++ app/settings.php
@@ -1,19 +1,11 @@
 ﻿<?php
 return $settings = array(	
 	'database'    => array(
-<<<<<<< HEAD
 				'type'		=> 'mysql',
 				'name'		=> 'bloggy_db',
 				'hostname'	=> '127.0.0.1',
 				'username'	=> 'root', 
 				'password'	=> '' 
-=======
-				'type'		=> 'database_type',
-				'name'		=> 'database_name',
-				'hostname'	=> 'localhost',
-				'username'	=> 'database_username', 
-				'password'	=> 'database_password' 
->>>>>>> 393cf50a775837579c35516a3f7e55303c6bdc4f
  		         ),		
 	'load_limit'  => 10,				 
 	'date_format' => date( 'j/n/Y' )
